ba29fe687d526d8d81f543ef531be924ba00e290
aditya95-pixel <sphs.am6@gmail.com>
2025-04-15 03:15:44 +0530

    completed builder's guide

--- aditmaz95.py
+++ aditmaz95.py
@@ -618,3 +618,25 @@ def k_fold(trainer, data, k, lr):
         models.append(model)
     print(f'average validation log mse = {sum(val_loss)/len(val_loss)}')
     return models
+
+def cpu():  
+    """Get the CPU device."""
+    return torch.device('cpu')
+
+def gpu(i=0):  
+    """Get a GPU device."""
+    return torch.device(f'cuda:{i}')
+
+def num_gpus(): 
+    """Get the number of available GPUs."""
+    return torch.cuda.device_count()
+
+def try_gpu(i=0):  
+    """Return gpu(i) if exists, otherwise return cpu()."""
+    if num_gpus() >= i + 1:
+        return gpu(i)
+    return cpu()
+
+def try_all_gpus():
+    """Return all available GPUs, or [cpu(),] if no GPU exists."""
+    return [gpu(i) for i in range(num_gpus())]
